b97677f2e3f78cbc0bbbb15011ec922110494e2b
daurenabasov <daurenabasov987@gmail.com>
2023-03-12 13:21:15 +0600

    feat/loader

--- src/Pages/Service/Service.jsx
+++ src/Pages/Service/Service.jsx
@@ -6,9 +6,18 @@ import Cards from "./ServiceComponents/ServiceCards/Cards";
 import ServiceHero from "./ServiceComponents/ServiceHero/ServiceHero";
 import Header from "../../Components/Layouts/Header/Header";
 import Footer from "../../Components/Layouts/Footer/Footer";
+import Loader from "../../Components/UI/Loader"
 
 const Service = () => {
   const [establishments, setEstablishments] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true)
+    }, 2000)
+  }, [])
+
 
   const getEstablishments = async () => {
     const res = await axios.get(API_ESTABLISHNENTS);
@@ -25,15 +34,19 @@ const Service = () => {
       <main id="container" className={s.ServiceContainer}>
         <ServiceHero />
         <div className={s.cards}>
-          {establishments.map((data) => (
-            <Cards
-              key={data.id}
-              id={data.id}
-              subtitle={data.subtitle}
-              photo={data.photo}
-              location={data.location}
-            />
-          ))}
+          {
+            loading ? (
+              establishments.map((data) => (
+                <Cards
+                  key={data.id}
+                  id={data.id}
+                  subtitle={data.subtitle}
+                  photo={data.photo}
+                  location={data.location}
+                />
+              ))
+            ) : (<Loader />)
+          }
         </div>
       </main>
       <Footer />
